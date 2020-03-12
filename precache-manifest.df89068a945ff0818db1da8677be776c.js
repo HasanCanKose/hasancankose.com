24eb6c9d52ccc1cdcc6c0ef4a0aef4ed92930f08
Hasan Can Kose <hasancannkosee@gmail.com>
2020-03-13 02:43:45 +0300

    Updates

---
+++ precache-manifest.df89068a945ff0818db1da8677be776c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c86c6a4778e5dc4583d8fdb6b47fb69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e669595959bee1fbab45",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93606643105ed950aa8f",
+    "url": "/static/js/2.0a0407e6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0a0407e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e669595959bee1fbab45",
+    "url": "/static/js/main.7e937556.chunk.js"
+  },
+  {
+    "revision": "b94c53c2c62dab98621c",
+    "url": "/static/js/runtime-main.f1a9e5a2.js"
+  }
+]);
