90049bda60f1301a1130bb40a998dae6e747eb3c
Hasan Can Kose <hasancannkosee@gmail.com>
2020-03-14 22:17:01 +0300

    js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Hasan Can Kose</h1>
+      <p>asjhdga</p>
+    </div>
+  );
+}
+
+export default Header;
